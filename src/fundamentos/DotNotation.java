a084382703dab06617273b6284ab0bd9dc4c95f3
Ana C. Santos Sousa <ana.c.santos.sousa@avanade.com>
2020-03-24 20:13:59 -0300

    dot notation

--- src/fundamentos/DotNotation.java
+++ src/fundamentos/DotNotation.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class DotNotation {
     public static void main(String[] args) {
         String s = "Bom dia X";
+        s = s.replace("X", "Camila");
         s = s.toUpperCase();
 
         System.out.println(s);
