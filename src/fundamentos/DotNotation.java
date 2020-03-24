f4d378c9cd9d04570a2cf3c040aed0ff7e116f23
Ana C. Santos Sousa <ana.c.santos.sousa@avanade.com>
2020-03-24 20:03:51 -0300

    new commit

---
+++ src/fundamentos/DotNotation.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class DotNotation {
+    public static void main(String[] args) {
+        String s = "Bom dia X";
+        s = s.toUpperCase();
+
+        System.out.println(s);
+    }
+}
